74a359d69975dde3ab53c146b25ed99ecf8cfe5e
shikhardutta-king <duttashikhar2006@gmail.com>
2026-07-22 22:28:36 +0530

    Create nextLine.cpp
    This repo contain code to make next line in c++

---
+++ nextLine.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Shikhar Dutta"<<endl;
+  cout<<"this guy is a legend";
+  cout<<"\n";
+  cout<<"or you can simpelly use \\n";
+  return 0;
+}
